5af61975337d55b1c78f2927851ce9cbd645da39
Alejandro Greco <alegreco@adinet.com.uy>
2025-11-03 15:03:44 -0300

    Un cambio simple

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,6 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         // Imprime "¡Hola, Mundo!" en la consola
-        System.out.println("¡Hola, Mundo!");
+        System.out.println("¡Hola, Mundo! ");
     }
 }
